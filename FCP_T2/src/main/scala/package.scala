7b5967d8366ed9b1db635d0788b6b77cc7911f37
Cristian David <cristian.pacheco@correounivalle.edu.co>
2023-09-19 09:58:50 -0500

    Adding the example function of how to create the binary operation

--- FCP_T2/src/main/scala/package.scala
+++ FCP_T2/src/main/scala/package.scala
@@ -12,11 +12,26 @@ def crearChipUnario ( f: Int => Int ) : Chip =
 
 def chip_not = crearchipunario( (x:Int) => (x - 1))
 
+  
+
 def crearChipBinario ( f: ( Int, Int ) => Int ) : Chip = {
 
+  // On every item in the currentList argument(in the head), apply the function f, and then call
+  // the operate function iteratively on the tail of the current list more the the result of the partial list
+  def operation(f, currentList, result) {
+            operation(f(currentList.head)::result, result)
+  }
+  // Return the function which do the binary oparation on every item of the list
+  (operands: List[Int]) => operator(f, operands, List()) 
 }
-def crearChipUnario ( f: Int => Int ) : Chip = {
 
+
+def chip_add = crearChipBinario( (x: Int, y: Int ) => (x * y))
+def chip_or = crearChipBinario( (x: Int, y: Int ) => (x + y) - (x * y ))
+
+
+
+def crearChipUnario ( f: Int => Int ) : Chip = {
 
 }
 
