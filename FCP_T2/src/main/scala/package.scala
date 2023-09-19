18f9c5c816f2494ead0dab4d9e7643b48988c91c
cristian_david_gentoo <cristian.pacheco@correounivalle.edu.co>
2023-09-18 23:51:59 -0500

    The method s templaltes

--- FCP_T2/src/main/scala/package.scala
+++ FCP_T2/src/main/scala/package.scala
@@ -1,3 +1,36 @@
 package object mypackage {
+
+     //The DSL(Domain Specific Language) Chip type declaration 
+     type Chip = List [Int] => List [Int]
       def hello = println("Hello, world!")
+
+     
+
+def crearChipUnario ( f: Int => Int ) : Chip =
+
+}
+
+def chip_not = crearchipunario( (x:Int) => (x - 1))
+
+def crearChipBinario ( f: ( Int, Int ) => Int ) : Chip = {
+
+}
+def crearChipUnario ( f: Int => Int ) : Chip = {
+
+
+}
+
+def halfadder : Chip = {
+
+}
+
+def fulladder : Chip = {
+
+}
+
+
+def adder ( n : Int ) : Chip = {}
+
+
+
 }
