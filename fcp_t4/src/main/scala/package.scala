b9a4bf973d8d0a6d12f0dcdcb7f0866dd180a682
Cristian David <cristian.pacheco@correounivalle.edu.co>
2023-10-22 11:50:42 -0700

    First try to implement the first pont, from the for exprestion to toSet collection

--- fcp_t4/src/main/scala/package.scala
+++ fcp_t4/src/main/scala/package.scala
@@ -1,4 +1,21 @@
 package object SubsecuenciaMasLarga {
     def hello = "Hello, world!"
     def bye = "Bye, world!"
+
+    type Secuencia = Seq[Int];
+    type Subsecuencia = Seq[Int];
+
+    def subindices(i: Int, n: Int): Set[Subsecuencia] = {
+        
+        def subsecuenceHelper(i: Int, n: Int, accSubsecuence: Set[Subsecuencia]): Set[Subsecuencia] = 
+            n match {
+                case n  if n == i => accSubsecuence
+                case _ => {
+                    val subsecuenceRange = i until n
+                    (for subsequenceIndex <- subsecuenceRange     
+                            yield subsecuenceHelper(i + 1, n, accSubsecuence + subsecuenceRange.filter( _ <= subsequenceIndex))).toSet
+                            
+                        }
+    }
+}
 }
